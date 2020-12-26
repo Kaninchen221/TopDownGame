86327275fd83903ed01fd821c3cf30f70bef9664
Paweł Krolik <pawel8krolik@gmail.com>
2020-12-26 17:23:21 +0100

    Create class for NPC

---
+++ Source/TopDownGame/TopDownNonPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TopDownCharacter.h"
+#include "TopDownNonPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNGAME_API ATopDownNonPlayerCharacter : public ATopDownCharacter
+{
+	GENERATED_BODY()
+	
+};
