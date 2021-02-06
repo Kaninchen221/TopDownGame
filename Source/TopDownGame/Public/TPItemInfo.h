10977a5a46a33821172c2bfc94613f0be1183630
Paweł Krolik <pawel8krolik@gmail.com>
2021-02-06 16:55:15 +0100

    Create TPItem detailed struct type for items

---
+++ Source/TopDownGame/Public/TPItemInfo.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TPItemInfo.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct TOPDOWNGAME_API FTPItemInfo {
+	
+	GENERATED_BODY()
+	
+public:
+
+	FTPItemInfo() = default;
+	~FTPItemInfo() noexcept = default;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ItemInfo")
+	int32 Id;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ItemInfo")
+	FText Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ItemInfo")
+	FText Description;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ItemInfo")
+	FString SpriteReference;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ItemInfo")
+	int32 BaseInGameValue;
+
+
+};
