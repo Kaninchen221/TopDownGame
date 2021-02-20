14470eceb073f3c60e5cf2dd01eb40ef4d7b2dcd
Paweł Krolik <pawel8krolik@gmail.com>
2021-02-20 10:23:15 +0100

    Add minimal settings

--- Source/DialogEditor/Private/Shared/DialogAssetEditorSettings.cpp
+++ Source/DialogEditor/Private/Shared/DialogAssetEditorSettings.cpp
@@ -3,6 +3,10 @@
 #include "Misc/Paths.h"
 
 UDialogAssetEditorSettings::UDialogAssetEditorSettings()
+	: BackgroundColor(FLinearColor::White)
+	, ForegroundColor(FLinearColor::Black)
+	, Font(FSlateFontInfo(FPaths::EngineContentDir() / TEXT("Slate/Fonts/DroidSansMono.ttf"), 10))
+	, Margin(4.0f)
 { 
 
 }
--- Source/DialogEditor/Public/Shared/DialogAssetEditorSettings.h
+++ Source/DialogEditor/Public/Shared/DialogAssetEditorSettings.h
@@ -16,4 +16,20 @@ public:
 
 	UDialogAssetEditorSettings();
 
+	/** Color of the TextAsset editor's background. */
+	UPROPERTY(config, EditAnywhere, Category = Appearance)
+	FSlateColor BackgroundColor;
+
+	/** Color of the TextAsset editor's text. */
+	UPROPERTY(config, EditAnywhere, Category = Appearance)
+	FSlateColor ForegroundColor;
+
+	/** The font to use in the TextAsset editor window. */
+	UPROPERTY(config, EditAnywhere, Category = Appearance)
+	FSlateFontInfo Font;
+
+	/** The margin around the TextAsset editor window (in pixels). */
+	UPROPERTY(config, EditAnywhere, Category = Appearance)
+	float Margin;
+
 };
