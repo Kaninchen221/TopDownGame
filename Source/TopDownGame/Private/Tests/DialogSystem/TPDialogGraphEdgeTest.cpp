b9814312b9bf27d99581e1629302d8af4c7b1195
Paweł Krolik <pawel8krolik@gmail.com>
2021-03-02 15:40:13 +0100

    Create DialogGraphEdge type for DialogGraph class

---
+++ Source/TopDownGame/Private/Tests/DialogSystem/TPDialogGraphEdgeTest.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CoreTypes.h"
+#include "Misc/AutomationTest.h"
+#include "Internationalization/Text.h"
+
+#include "DialogSystem/TPDialogGraphEdge.h"
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FDialogGraphEdgeTest, "Project.UnitTests.DialogSystem.UTPDialogGraphEdge", EAutomationTestFlags::EditorContext | EAutomationTestFlags::SmokeFilter)
+
+bool FDialogGraphEdgeTest::RunTest(const FString& Parameters)
+{
+    auto CreateGraphNode = [&]() -> UTPDialogGraphEdge* {
+        UTPDialogGraphEdge* DialogGraphEdge = NewObject<UTPDialogGraphEdge>();
+        TestNotNull("DialogGraphEdge can't be null", DialogGraphEdge);
+        return DialogGraphEdge;
+    };
+
+    {
+        UTPDialogGraphEdge* DialogGraphEdge = CreateGraphNode();
+    }
+
+    return true;
+}
+
+#endif /// WITH_DEV_AUTOMATION_TESTS
---
+++ Source/TopDownGame/Public/DialogSystem/TPDialogGraphEdge.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericGraphEdge.h"
+#include "TPDialogGraphEdge.generated.h"
+
+
+UCLASS(Blueprintable)
+class TOPDOWNGAME_API UTPDialogGraphEdge : public UGenericGraphEdge
+{
+	GENERATED_BODY()
+
+public:
+
+};
