16ff88b48d8cce98318f01ec1602960c8c9fbd84
Paweł Krolik <pawel8krolik@gmail.com>
2021-03-11 16:17:07 +0100

    Better if

--- Source/DialogEditor/Private/TPAssetEditor_DialogGraph.cpp
+++ Source/DialogEditor/Private/TPAssetEditor_DialogGraph.cpp
@@ -1,10 +1,11 @@
 #include "TPAssetEditor_DialogGraph.h"
+#include "DialogEditor.h"
 
 void FTPAssetEditor_DialogGraph::SaveAsset_Execute()
 {
 	UE_LOG(DialogEditor, Log, TEXT("FAssetEditor_DialogGraph::SaveAsset_Execute"));
 
-	if (EditingGraph != nullptr)
+	if (EditingGraph)
 	{
 		RebuildGenericGraph();
 	}
