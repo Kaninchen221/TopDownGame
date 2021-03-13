9eb82a72b37a3d21929291e35fd247631f0e14ca
Paweł Krolik <pawel8krolik@gmail.com>
2021-03-13 18:54:10 +0100

    Use FTPDialogGraphValidator to validate editing graph

--- Source/DialogEditor/Private/TPAssetEditor_DialogGraph.cpp
+++ Source/DialogEditor/Private/TPAssetEditor_DialogGraph.cpp
@@ -1,13 +1,29 @@
 #include "TPAssetEditor_DialogGraph.h"
 #include "DialogEditor.h"
+#include "TPDialogGraphValidator.h"
+#include "DialogSystem/TPDialogGraph.h"
+
+#include "Misc/MessageDialog.h"
 
 void FTPAssetEditor_DialogGraph::SaveAsset_Execute()
 {
-	UE_LOG(DialogEditor, Log, TEXT("FAssetEditor_DialogGraph::SaveAsset_Execute"));
-
-	if (EditingGraph)
+	UTPDialogGraph* DialogGraph = Cast<UTPDialogGraph>(EditingGraph);
+	if (DialogGraph)
 	{
 		RebuildGenericGraph();
+
+		FText Result = FTPDialogGraphValidator::ValidateGraph(DialogGraph);
+		if (Result.IsEmpty())
+		{
+			FAssetEditorToolkit::SaveAsset_Execute();
+		}
+		else
+		{
+			FMessageDialog::Open(EAppMsgType::Ok, Result);
+		}
+	}
+	else
+	{
+		FMessageDialog::Open(EAppMsgType::Ok, FText::FromString("FTPAssetEditor_DialogGraph::SaveAsset_Execute EditingGraph is not a UTPDialogGraph type"));
 	}
 }
-
