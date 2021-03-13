eb1c935a294031e92b9f64e34840a36b8ba38ee3
Paweł Krolik <pawel8krolik@gmail.com>
2021-03-13 11:27:43 +0100

    Mock class for TPDialogGraphNode

---
+++ Source/DialogEditor/Private/Test/TPDialogGraphNodeMock.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "DialogSystem/TPDialogGraphNode.h"
+#include "TPDialogGraphNodeMock.generated.h"
+
+UCLASS()
+class UTPDialogGraphNodeMock : public UTPDialogGraphNode
+{
+	GENERATED_BODY()
+
+public:
+
+#if WITH_EDITOR
+
+	inline virtual bool CanCreateConnection(UGenericGraphNode* Other, FText& ErrorMessage) override;
+
+#endif WITH_EDITOR
+
+	inline int32 GetCanCreateConnectionCallCount() const;
+
+private:
+
+	int32 CanCreateConnectionCallCount = 0;
+
+};
+
+#if WITH_EDITOR
+
+bool UTPDialogGraphNodeMock::CanCreateConnection(UGenericGraphNode* Other, FText& ErrorMessage)
+{
+	CanCreateConnectionCallCount++;
+
+	return UTPDialogGraphNode::CanCreateConnection(Other, ErrorMessage);
+}
+
+#endif WITH_EDITOR
+
+int32 UTPDialogGraphNodeMock::GetCanCreateConnectionCallCount() const
+{
+	return CanCreateConnectionCallCount;
+}
